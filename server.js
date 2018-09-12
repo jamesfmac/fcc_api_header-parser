38b6478defecff0b8c5767545d0833b042437c23
Glitch (salty-recess) <none>
2018-09-12 07:07:20 +0000

    🚈🏌 Checkpoint ./server.js:1747620/149

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get("/api/hello", function (req, res) {
 app.get('/api/whoami', function(req,res){
   console.log(req)
   
-  res.json({ipaddress: '', language: '', software: ''})
+  res.json({ipaddress: req.headers['x-forwarded-for'], language: req.headers['accept-language'], software: req.headers['user-agent']})
   
 }
 )
